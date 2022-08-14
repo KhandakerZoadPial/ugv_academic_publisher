9facfa04f1d8ec0dce9d9d9bb5c108958d4b3561
KhandakerZoadPial <khpial27@gmail.com>
2022-08-15 01:47:41 +0600

    project initialized and basic database models created

--- publisher/models.py
+++ publisher/models.py
@@ -1,10 +1,13 @@
-from ast import mod
 from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(models.model):
-    profile_of = models.ForeignKey(User, on_delete=models.CASCADE)
+class FileHolder(models.Model):
+    file = models.FileField(blank=True, null=True, upload_to='documents')
+
+
+class UserProfile(models.Model):
+    profile_of = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.TextField(blank=True)
     gender = models.TextField(blank=True)
     type_of_profile = models.IntegerField(blank=True, null=True) # 1=Teacher 2=Student
@@ -12,3 +15,10 @@ class UserProfile(models.model):
 
     def __str__(self):
         return str(self.full_name)
+
+
+class ProjectPublication(models.Model):
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    type_of = models.IntegerField(blank=True, null=True) # 1=project 2=publication
+    files = models.ManyToManyField(FileHolder, blank=True, null=True, related_name='files')
+    collaborators = models.ManyToManyField(UserProfile, blank=True, null=True, related_name='colaborators')
